aa9d27352e77eb1bfdadd669ebcf9708abe5e355
Carlos Enrique Moctezuma Avalos <carlos.moctezuma@iest.edu.mx>
2026-07-30 18:17:11 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-0a91e3c5-2792-4632-a70e-0f29eef817a9.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
